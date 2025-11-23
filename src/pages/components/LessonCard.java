9e3a8bf45f48ed65408cab03cdbf21d0581a4def
Abdoalrahmankhedr <abdoalrahmankhedr95@gmail.com>
2025-11-23 15:49:40 +0200

    Mange the course lesson card

--- src/pages/components/LessonCard.java
+++ src/pages/components/LessonCard.java
@@ -119,7 +119,8 @@ public class LessonCard extends JPanel {
                 }
                 attemptsHistoryBtn.addActionListener(historyAction);
             }
-        } else if(state) {
+        }
+        else if(state) {
             takeQuizBtn.setEnabled(false);
             takeQuizBtn.setText("No QUIZ");
             takeQuizBtn.setVisible(true);
